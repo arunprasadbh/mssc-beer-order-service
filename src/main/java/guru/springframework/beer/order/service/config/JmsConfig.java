34afb54b68ff4f920dbb538a1515d2d48ec9f8a4
arunabhamidipati <arunprasad_bh@yahoo.com>
2019-12-13 06:50:36 +0000

    Add JMS configuration. closes #6

---
+++ src/main/java/guru/springframework/beer/order/service/config/JmsConfig.java
@@ -0,0 +1,25 @@
+package guru.springframework.beer.order.service.config;
+
+/*
+ * Created by arunabhamidipati on 13/12/2019
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    public static final String orderQueue = "ORDER_QUEUE";
+
+    @Bean
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
